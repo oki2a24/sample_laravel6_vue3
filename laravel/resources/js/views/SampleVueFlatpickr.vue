fc99344654174754a99b968d5a0be7a56ebbfa85
oki2a24 <oki2a24@gmail.com>
2021-06-06 06:25:55 +0900

    feat: ankurk91/vue-flatpickr-component を追加して使用例も追加する

---
+++ laravel/resources/js/views/SampleVueFlatpickr.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+  <h1>SampleVueFlatpickr</h1>
+  <flat-pickr v-model="date"></flat-pickr>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import flatPickr from "vue-flatpickr-component";
+import "flatpickr/dist/flatpickr.css";
+
+export default {
+  name: "SampleVueFlatpickr",
+  components: { flatPickr },
+  setup() {
+    const date = ref("");
+    return { date };
+  },
+};
+</script>
