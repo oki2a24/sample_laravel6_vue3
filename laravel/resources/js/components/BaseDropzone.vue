6901911b5ed67df227a00b0b4bdc94001ba87f82
oki2a24 <oki2a24@gmail.com>
2021-06-05 11:47:46 +0900

    feat: Dropzone.js の文言に日本語を設定、ファイルの除去ボタン追加、アップロード成功ファイルのフェードアウトを追加する

--- laravel/resources/js/components/BaseDropzone.vue
+++ laravel/resources/js/components/BaseDropzone.vue
@@ -19,9 +19,24 @@ export default {
     const initDropzone = () => {
       Dropzone.autoDiscover = false;
       //new Dropzone("div#myId", { url: "https://httpbin.org/post" });
-      const myDropzone = new Dropzone("div#myDropzoneId", { url: props.url });
+      const myDropzone = new Dropzone("div#myDropzoneId", {
+        url: props.url,
+        addRemoveLinks: true,
+        dictDefaultMessage:
+          "画像ファイルをここにドロップするか、クリックしてアップロード",
+        dictCancelUpload: "アップロードをキャンセル",
+        dictRemoveFile: "操作エリアから除去",
+      });
       myDropzone.on("success", (file, response) => {
         emit("onSuccess", file, response);
+        setTimeout(() => myDropzone.removeFile(file), 10000);
+      });
+      myDropzone.on("error", function (file, errorMessage) {
+        console.log("error", file, errorMessage); // TODO デバッグ用途。後で消すこと。
+        // レスポンスの内容を使用するときは、エラーメッセージの後ろに付与すればよさそう。
+        file.previewTemplate.getElementsByClassName(
+          "dz-error-message"
+        )[0].textContent = "アップロードに失敗しました。";
       });
     };
 
@@ -32,6 +47,13 @@ export default {
 };
 </script>
 
+<style>
+.dz-success {
+  transition: opacity 10s, visibility 0s ease 10s;
+  opacity: 0;
+  visibility: hidden;
+}
+</style>
 <!--
 ここでスタイルを読み込むことも可能
 <style src="../../../node_modules/dropzone/dist/dropzone.css"></style>
